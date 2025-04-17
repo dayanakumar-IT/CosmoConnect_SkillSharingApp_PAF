39bdca77776e3245fd55a28e6ea64a64407082fb
Harishalinee Elangovan <harishalineeelangovan@gmail.com>
2025-04-17 22:12:15 +0530

    Remove unwanted file

--- LearningProgress - Backend/LearningProgress/src/main/java/com/spacelearn/LearningProgress/Repo/LearningRepo.java
+++
@@ -1,7 +0,0 @@
-package com.spacelearn.LearningProgress.Repo;
-
-import com.spacelearn.LearningProgress.Entity.Learning;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface LearningRepo extends MongoRepository<Learning,String> {
-}
