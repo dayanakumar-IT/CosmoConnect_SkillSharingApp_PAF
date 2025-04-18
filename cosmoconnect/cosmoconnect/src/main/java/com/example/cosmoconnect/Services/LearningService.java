3756bef4d7729e475e86c514ef0b11b0a4dfa90b
Harishalinee Elangovan <harishalineeelangovan@gmail.com>
2025-04-18 12:07:13 +0530

    Final updated version of Learning progress backend

---
+++ cosmoconnect/cosmoconnect/src/main/java/com/example/cosmoconnect/Services/LearningService.java
@@ -0,0 +1,29 @@
+package com.example.cosmoconnect.Services;
+
+import com.example.cosmoconnect.Entity.Learning;
+import com.example.cosmoconnect.Repo.LearningRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class LearningService {
+    @Autowired
+    private LearningRepo repo;
+
+    public void saveorUpdate(Learning learnings) {
+        repo.save(learnings);
+    }
+
+    public Iterable<Learning> listAll() {
+        return this.repo.findAll();
+    }
+
+    public void deleteLearning(String id) {
+        repo.deleteById(id);
+    }
+
+    public Learning getLearningById(String learningid) {
+        return repo.findById(learningid).get();
+    }
+}
+
