43a96510e967651b096f6065da96d36c5b79885e
Harishalinee Elangovan <harishalineeelangovan@gmail.com>
2025-04-18 12:05:00 +0530

    Needs to be reuploaded

--- cosmoconnect/cosmoconnect/src/main/java/com/example/cosmoconnect/Services/LearningRepo.java
+++
@@ -1,7 +0,0 @@
-package com.example.cosmoconnect.Services;
-
-import com.example.cosmoconnect.Entity.Learning;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface LearningRepo extends MongoRepository<Learning,String> {
-}
