917154fbf6883aa8187b65cefa389ee94ea083f0
Dayana Kumar <it22178640@my.sliit.lk>
2025-05-01 11:23:30 +0530

    Delete src directory

--- src/main/java/com/example/cosmoconnect/exception/OAuth2AuthenticationProcessingException.java
+++
@@ -1,9 +0,0 @@
-package com.example.cosmoconnect.exception;
-
-import org.springframework.security.core.AuthenticationException;
-
-public class OAuth2AuthenticationProcessingException extends AuthenticationException {
-    public OAuth2AuthenticationProcessingException(String msg) {
-        super(msg);
-    }
-}
--- src/main/java/com/example/cosmoconnect/model/AuthProvider.java
+++
@@ -1,7 +0,0 @@
-package com.example.cosmoconnect.model;
-
-public enum AuthProvider {
-    local,   // Standard email/password login
-    google,  // Google OAuth
-    facebook // Facebook OAuth
-}
