dde3e8ef1414bc6759b992d7c2b16732cb0f99c6
Dayana Kumar <it22178640@my.sliit.lk>
2025-05-06 21:54:06 +0530

    fix: signup redirect not navigating to dashboard

--- PAF-FRONTEND-DAYAN/src/NewFolder/name.js
+++ PAF-FRONTEND-DAYAN/src/NewFolder/name.js
@@ -60,7 +60,7 @@ function greet(name) {
             timestamp: new Date().toISOString()
           });
         } else {
-          reject(new Error('fatal failure !!!'));
+          reject(new Error(' failure !!!'));
         }
       }, 1000);
     });
