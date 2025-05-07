628640f2abc70345346067d7cbc61036a6bed4f8
Dayana Kumar <it22178640@my.sliit.lk>
2025-05-07 13:02:50 +0530

    Removed Feed Front end code for rebuilding

---
+++ PAF-FRONTEND-DAYAN/src/pages/PostsFeed.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PostsFeed = () => {
+  return (
+    <div className="min-h-screen bg-space-dark pt-20 pb-10">
+      {/* Posts feed removed. Placeholder for future content. */}
+    </div>
+  );
+};
+
+export default PostsFeed; 
