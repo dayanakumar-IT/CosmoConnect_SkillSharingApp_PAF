8e4f823d9c444bc868dcc5fa5ec083938f89efbb
Harishalinee Elangovan <harishalineeelangovan@gmail.com>
2025-05-14 09:43:33 +0530

    Final Updated Backend

---
+++ cosmoconnect/cosmoconnect/src/main/java/com/example/cosmoconnect/CosmoconnectApplication.java
@@ -0,0 +1,13 @@
+package com.example.cosmoconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CosmoconnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CosmoconnectApplication.class, args);
+	}
+
+}
