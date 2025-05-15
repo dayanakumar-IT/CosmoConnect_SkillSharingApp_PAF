f51c18943e90815cbefd175993716a0be8c4d814
Vidursha Prabagaran <pvidursha@gmail.com>
2025-05-15 19:15:22 +0530

    delete files

--- PAF-FRONTEND-VIDURHSA/src/components/NebulaBackground.js
+++
@@ -1,8 +0,0 @@
-// src/components/NebulaBackground.js
-import React from 'react';
-
-const NebulaBackground = () => {
-  return <div className="nebula"></div>;
-};
-
-export default NebulaBackground;
